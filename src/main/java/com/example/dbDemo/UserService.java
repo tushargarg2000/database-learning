28ce4fcc86cdb24e58519055b3a21ef03eb5fae9
Tushar <tushargarg0110@gmail.com>
2023-08-08 23:00:55 +0530

    apis

--- src/main/java/com/example/dbDemo/UserService.java
+++ src/main/java/com/example/dbDemo/UserService.java
@@ -1,7 +1,13 @@
 package com.example.dbDemo;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +24,52 @@ public class UserService {
 
     }
 
+    public UserInfo getUserById(Integer userId)throws Exception{
+
+        Optional<UserInfo> optionalUserInfo = userRepository.findById(userId);
+
+        if(!optionalUserInfo.isPresent()){
+             throw new Exception("User with Id is not present");
+        }
+
+        UserInfo userInfo = optionalUserInfo.get();
+
+        return userInfo;
+
+    }
+
+    public String deleteUserById(Integer id){
+        if(userRepository.existsById(id)==true){
+            userRepository.deleteById(id);
+            return "User with Id is deleted";
+        }else {
+            return "UserId was invalid";
+        }
+    }
+
+    public String updateEmail(Integer userId,String newEmail){
+
+        Optional<UserInfo> optionalUserInfo = userRepository.findById(userId);
+
+        //Handle that not present case : Exception handling should be there
+
+        UserInfo userInfo = optionalUserInfo.get();
+        userInfo.setEmail(newEmail);
+        userRepository.save(userInfo);
+        return "Email updated successfully";
+    }
 
 
+    public List<UserInfo> getUsersAboveAge(Integer age)
+    {
+        List<UserInfo> allUsers = userRepository.findAll();
+        List<UserInfo> ansList = new ArrayList<>();
 
+        for(UserInfo userInfo : allUsers){
+            if(userInfo.getAge()>age){
+                ansList.add(userInfo);
+            }
+        }
+        return ansList;
+    }
 }
--- src/main/java/com/example/dbDemo/Usercontroller.java
+++ src/main/java/com/example/dbDemo/Usercontroller.java
@@ -1,8 +1,15 @@
 package com.example.dbDemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +19,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class Usercontroller {
 
     @Autowired
     UserService userService;
 
     @GetMapping("/getById")
-    public String getUserById(@RequestParam("id")Integer id){
+    public ResponseEntity getUserById(@RequestParam("id")Integer id){
 
-
-        return "";
+        try {
+            UserInfo userInfo = userService.getUserById(id);
+            return new ResponseEntity(userInfo, HttpStatus.OK);
+        }catch (Exception e){
+            log.error("This is an error and "+e.getMessage());
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
 
     }
 
@@ -30,4 +43,21 @@ public class Usercontroller {
         return userService.addUser(userInfo);
     }
 
+
+    @DeleteMapping("/deleteById")
+    public String deleteById(@RequestParam("id")Integer id){
+        return userService.deleteUserById(id);
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(@RequestParam("id")Integer id,@RequestParam("email")String email){
+
+        return userService.updateEmail(id,email);
+    }
+
+    @GetMapping("/findUserAboveAge")
+    public List<UserInfo> getUserInfoList(@RequestParam("age")Integer age){
+
+
+    }
 }
